6d2654a4a41fd344be1677cfdd796f371ecff565
saiherng <saiherng14@gmail.com>
2021-04-04 09:52:26 -0700

    added datatable with search filter functionality

--- main/models.py
+++ main/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+#from djmoney.models.fields import MoneyField
+
 
 # Create your models here.
 
@@ -7,8 +9,12 @@ class Item(models.Model):
 
     name = models.CharField(max_length=50)
     quantity = models.IntegerField()
-    retail_price = models.DecimalField(max_digits=8, decimal_places=2)
+    retail_price = models.DecimalField(max_digits=10, decimal_places=2)
     buy_date = models.DateTimeField()
+
+    @property
+    def getPrice(self):
+        return str(self.retail_price) + " Kyats"
 
     def __str__(self):
         return self.name
